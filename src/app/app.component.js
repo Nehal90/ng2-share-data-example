12dcf93989e556a9e4d8fbb7499595c10a6c5fbb
Nehal <an425@live.com>
2017-05-08 22:26:55 -0400

    -setup hero list and detail components from angular.io

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -8,14 +8,13 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
-        this.name = 'Angular';
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "<h1>Hello {{name}}</h1>",
+        template: "\n  <div class=\"container\">\n    <h1>Reactive Forms</h1>\n    <hero-list></hero-list>\n  </div>"
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
--- src/app/app.module.js
+++ src/app/app.module.js
@@ -7,7 +7,11 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
+var forms_1 = require("@angular/forms"); // <-- #1 import module
 var app_component_1 = require("./app.component");
+var hero_detail_component_1 = require("./hero-detail.component"); // <-- #1 import component
+var hero_list_component_1 = require("./hero-list.component");
+var hero_service_1 = require("./hero.service"); //  <-- #1 import service
 var AppModule = (function () {
     function AppModule() {
     }
@@ -15,8 +19,21 @@ var AppModule = (function () {
 }());
 AppModule = __decorate([
     core_1.NgModule({
-        imports: [platform_browser_1.BrowserModule],
-        declarations: [app_component_1.AppComponent],
+        imports: [
+            platform_browser_1.BrowserModule,
+            forms_1.ReactiveFormsModule // <-- #2 add to Angular module imports
+        ],
+        declarations: [
+            app_component_1.AppComponent,
+            hero_detail_component_1.HeroDetailComponent,
+            hero_list_component_1.HeroListComponent
+        ],
+        exports: [
+            app_component_1.AppComponent,
+            hero_detail_component_1.HeroDetailComponent,
+            hero_list_component_1.HeroListComponent
+        ],
+        providers: [hero_service_1.HeroService],
         bootstrap: [app_component_1.AppComponent]
     })
 ], AppModule);
